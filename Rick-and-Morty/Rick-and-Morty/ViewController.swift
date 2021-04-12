e6e8ca8d8e5187965a7e8da51def52b574fbbc2a
Alterego11 <alterego15071970@gmail.com>
2021-04-12 22:52:26 +0300

    Create navigation

---
+++ Rick-and-Morty/Rick-and-Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick-and-Morty
+//
+//  Created by Антон Белый on 12.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
