6b701a5f343f5b691ffc4c444e8121973924e863
Alterego11 <alterego15071970@gmail.com>
2021-06-02 17:36:49 +0300

    Add Stretchy Header

--- Rick-and-Morty/Rick-and-Morty/View/CharactersVC/CharactersVC.swift
+++ Rick-and-Morty/Rick-and-Morty/View/CharactersVC/CharactersVC.swift
@@ -9,30 +9,31 @@ import UIKit
 
 class CharactersVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let model = [
-        "Primer1",
-        "Primer2",
-        "Primer3",
-        "Primer4",
-        "Primer5",
-        "Primer6",
-        "Primer7",
-        "Primer1",
-        "Primer2",
-        "Primer3",
-        "Primer4",
-        "Primer5",
-        "Primer6",
-        "Primer7",
-        "Primer2",
-        "Primer3",
-        "Primer4",
-        "Primer5",
-        "Primer6",
-        "Primer7"
+    let example = [
+        "Primer 1",
+        "Primer 2",
+        "Primer 3",
+        "Primer 4",
+        "Primer 5",
+        "Primer 6",
+        "Primer 7",
+        "Primer 8",
+        "Primer 9",
+        "Primer 10",
+        "Primer 11",
+        "Primer 12",
+        "Primer 13",
+        "Primer 14",
+        "Primer 15",
+        "Primer 16",
+        "Primer 17",
+        "Primer 18",
+        "Primer 19",
+        "Primer 20",
+        "Primer 21",
+        
     ]
-    
-    
+    // Создаем TableView
     private let tableView: UITableView = {
        let tableView = UITableView()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
@@ -45,32 +46,29 @@ class CharactersVC: UIViewController, UITableViewDelegate, UITableViewDataSource
         tableView.delegate = self
         tableView.dataSource = self
         tableView.frame = view.bounds
-        
-        
-        
-
-
-//        let header = StretchyTableHeaderView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.width))
-//        header.imageView.image = #imageLiteral(resourceName: "head")
-//        tableView.tableHeaderView = header
+        //Настраиваем Header
+        let headerView = StretchyHeaderCharactersVC(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 250))
+        headerView.imageView.image = UIImage(named: "morty")
+        self.tableView.tableHeaderView = headerView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return model.count
+        return example.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = model[indexPath.row]
+        cell.textLabel?.text = example[indexPath.row]
         return cell
     }
     
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let headerImage: UIImage = UIImage(named: "head")!
-        let headerView = UIImageView(image: headerImage)
-        return headerView
-    
 }
+
+extension CharactersVC: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let headerView = self.tableView.tableHeaderView as! StretchyHeaderCharactersVC
+        headerView.scrollViewDidScroll(scrollView: scrollView)
+    }
 }
 
 
--- Rick-and-Morty/Rick-and-Morty/View/CharactersVC/StretchyHeaderCharactersVC.swift
+++ Rick-and-Morty/Rick-and-Morty/View/CharactersVC/StretchyHeaderCharactersVC.swift
@@ -1,5 +1,5 @@
 //
-//  StretchyTableHeaderView.swift
+//  StretchyHeaderCharactersVC.swift
 //  Rick-and-Morty
 //
 //  Created by Антон Белый on 01.06.2021.
@@ -7,50 +7,52 @@
 
 import UIKit
 
-final class StretchyTableHeaderView: UIView {
-    public let imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.clipsToBounds = true
-        imageView.contentMode = .scaleAspectFill
-        return imageView
-    }()
+final class StretchyHeaderCharactersVC: UIView {
+
+    var imageViewHeight = NSLayoutConstraint()
+    var imageViewBottom = NSLayoutConstraint()
     
+    var containerView: UIView!
+    var imageView: UIImageView!
     
-    private var imageViewHeight = NSLayoutConstraint()
-    private var imageViewBottom = NSLayoutConstraint()
-    private var containerViewHeight = NSLayoutConstraint()
-    private var containerView = UIView()
-    
-    // MARK: INIT
+    var containerViewHeight = NSLayoutConstraint()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-//        createView()
-        //setViewConstraints()
+        createView()
+        setViewConstraints()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
-    private func createView() {
-        addSubview(containerView)
+    func createView() {
+        //View контейнера
+        containerView = UIView()
+        self.addSubview(containerView)
+        //ImageView для фона
+        imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .black
+        imageView.contentMode = .scaleAspectFit
         containerView.addSubview(imageView)
     }
     
     func setViewConstraints() {
+        //Констрейнты для UIView
         NSLayoutConstraint.activate([
-            widthAnchor.constraint(equalTo: containerView.widthAnchor),
-            centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
-            heightAnchor.constraint(equalTo: containerView.heightAnchor)
+            self.widthAnchor.constraint(equalTo: containerView.widthAnchor),
+            self.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            self.heightAnchor.constraint(equalTo: containerView.heightAnchor)
         ])
-        
+        //Констрейнты для ContainerView
         containerView.translatesAutoresizingMaskIntoConstraints = false
-        
         containerView.widthAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
         containerViewHeight = containerView.heightAnchor.constraint(equalTo: self.heightAnchor)
         containerViewHeight.isActive = true
         
+        //Констрейнты для ImageView
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageViewBottom = imageView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
         imageViewBottom.isActive = true
@@ -58,11 +60,15 @@ final class StretchyTableHeaderView: UIView {
         imageViewHeight.isActive = true
     }
     
-    public func scrollviewDidScroll(scrollView: UIScrollView) {
+    func scrollViewDidScroll(scrollView: UIScrollView) {
         containerViewHeight.constant = scrollView.contentInset.top
         let offsetY = -(scrollView.contentOffset.y + scrollView.contentInset.top)
         containerView.clipsToBounds = offsetY <= 0
         imageViewBottom.constant = offsetY >= 0 ? 0 : -offsetY / 2
         imageViewHeight.constant = max(offsetY + scrollView.contentInset.top, scrollView.contentInset.top)
     }
+    
+    
 }
+    
+
