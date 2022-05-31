e8677e950b7c977caa2805e58f1e774b2ac75890
Alterego11 <>
2022-05-31 21:30:10 +0300

    Fix

--- Rick-and-Morty/Rick-and-Morty/Network/Networking/Service/NetworkRouter.swift
+++ Rick-and-Morty/Rick-and-Morty/Network/Networking/Service/NetworkRouter.swift
@@ -10,7 +10,9 @@ import Foundation
 public typealias NetworkRouterCompletion = (_ data: Data?, _ response: URLResponse?, _ error: Error?) -> ()
 
 protocol NetworkRouter: AnyObject {
+    
     associatedtype EndPoint: EndPointType
     func request(_ route: EndPoint, completion: @escaping NetworkRouterCompletion)
     func cancel()
+    
 }
